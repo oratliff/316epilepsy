756880cda91b5768e9c54a872c2bdffefa0f65e6
Kate Chen <kzc3@duke.edu>
2020-03-05 12:34:18 -0500

    added pipfile, removed flask beers

--- config.py
+++
@@ -1,3 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgres://prbxpoeaihkxly:f83c6578489316eb2387f126f47bea98398521aa4e74f4b4e5f0de06f48746f7@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d2k8ntsq38d25h'
-SQLALCHEMY_ECHO = True
-DEBUG = True
