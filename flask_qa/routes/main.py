3fb753018bd47e6ead52a3e5ae1cce9aeeaa3e08
Kate Chen <kzc3@duke.edu>
2020-03-20 00:27:36 -0400

    changed home to login

--- flask_qa/routes/main.py
+++ flask_qa/routes/main.py
@@ -6,112 +6,146 @@ from flask_qa.models import Question, User
 
 main = Blueprint('main', __name__)
 
-# @main.route('/')
-# def index():
-#     questions = Question.query.filter(Question.answer != None).all()
-
-#     context = {
-#         'questions' : questions
-#     }
-
-#     return render_template('home.html', **context)
-
-@main.route('/ask', methods=['GET', 'POST'])
-@login_required
-def ask():
+@main.route('/', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        question = request.form['question']
-        expert = request.form['expert']
+        name = request.form['name']
+        password = request.form['password']
 
-        question = Question(
-            question=question, 
-            expert_id=expert, 
-            asked_by_id=current_user.id
+        user = User.query.filter_by(name=name).first()
+
+        error_message = ''
+
+        if not user or not check_password_hash(user.password, password):
+            error_message = 'Could not login. Please check and try again.'
+
+        if not error_message:
+            login_user(user)
+            return redirect(url_for('main.login'))
+
+    return render_template('login.html')
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        unhashed_password = request.form['password']
+
+        user = User(
+            name=name, 
+            unhashed_password=unhashed_password,
+            admin=False,  
+            expert=False
         )
 
-        db.session.add(question)
+        db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.login'))
 
-    experts = User.query.filter_by(expert=True).all()
+    return render_template('register.html')
 
-    context = {
-        'experts' : experts
-    }
+@main.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
 
-    return render_template('ask.html', **context)
+# @main.route('/ask', methods=['GET', 'POST'])
+# @login_required
+# def ask():
+#     if request.method == 'POST':
+#         question = request.form['question']
+#         expert = request.form['expert']
 
-@main.route('/answer/<int:question_id>', methods=['GET', 'POST'])
-@login_required
-def answer(question_id):
-    if not current_user.expert:
-        return redirect(url_for('main.index'))
+#         question = Question(
+#             question=question, 
+#             expert_id=expert, 
+#             asked_by_id=current_user.id
+#         )
 
-    question = Question.query.get_or_404(question_id)
+#         db.session.add(question)
+#         db.session.commit()
 
-    if request.method == 'POST':
-        question.answer = request.form['answer']
-        db.session.commit()
+#         return redirect(url_for('main.login'))
 
-        return redirect(url_for('main.unanswered'))
+#     experts = User.query.filter_by(expert=True).all()
 
-    context = {
-        'question' : question
-    }
+#     context = {
+#         'experts' : experts
+#     }
 
-    return render_template('answer.html', **context)
+#     return render_template('ask.html', **context)
 
-@main.route('/question/<int:question_id>')
-def question(question_id):
-    question = Question.query.get_or_404(question_id)
+# @main.route('/answer/<int:question_id>', methods=['GET', 'POST'])
+# @login_required
+# def answer(question_id):
+#     if not current_user.expert:
+#         return redirect(url_for('main.login'))
 
-    context = {
-        'question' : question
-    }
+#     question = Question.query.get_or_404(question_id)
 
-    return render_template('question.html', **context)
+#     if request.method == 'POST':
+#         question.answer = request.form['answer']
+#         db.session.commit()
 
-@main.route('/unanswered')
-@login_required
-def unanswered():
-    if not current_user.expert:
-        return redirect(url_for('main.index'))
+#         return redirect(url_for('main.unanswered'))
 
-    unanswered_questions = Question.query\
-        .filter_by(expert_id=current_user.id)\
-        .filter(Question.answer == None)\
-        .all()
+#     context = {
+#         'question' : question
+#     }
 
-    context = {
-        'unanswered_questions' : unanswered_questions
-    }
+#     return render_template('answer.html', **context)
 
-    return render_template('unanswered.html', **context)
+# @main.route('/question/<int:question_id>')
+# def question(question_id):
+#     question = Question.query.get_or_404(question_id)
 
-@main.route('/users')
-@login_required
-def users():
-    if not current_user.admin:
-        return redirect(url_for('main.index'))
+#     context = {
+#         'question' : question
+#     }
 
-    users = User.query.filter_by(admin=False).all()
+#     return render_template('question.html', **context)
 
-    context = {
-        'users' : users
-    }
+# @main.route('/unanswered')
+# @login_required
+# def unanswered():
+#     if not current_user.expert:
+#         return redirect(url_for('main.login'))
 
-    return render_template('users.html', **context)
+#     unanswered_questions = Question.query\
+#         .filter_by(expert_id=current_user.id)\
+#         .filter(Question.answer == None)\
+#         .all()
 
-@main.route('/promote/<int:user_id>')
-@login_required
-def promote(user_id):
-    if not current_user.admin:
-        return redirect(url_for('main.index'))
+#     context = {
+#         'unanswered_questions' : unanswered_questions
+#     }
 
-    user = User.query.get_or_404(user_id)
+#     return render_template('unanswered.html', **context)
 
-    user.expert = True
-    db.session.commit()
+# @main.route('/users')
+# @login_required
+# def users():
+#     if not current_user.admin:
+#         return redirect(url_for('main.login'))
 
-    return redirect(url_for('main.users'))
+#     users = User.query.filter_by(admin=False).all()
+
+#     context = {
+#         'users' : users
+#     }
+
+#     return render_template('users.html', **context)
+
+# @main.route('/promote/<int:user_id>')
+# @login_required
+# def promote(user_id):
+#     if not current_user.admin:
+#         return redirect(url_for('main.login'))
+
+#     user = User.query.get_or_404(user_id)
+
+#     user.expert = True
+#     db.session.commit()
+
+#     return redirect(url_for('main.users'))
